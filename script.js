79803ca7cbc933f971fcd90dbfdf878a2be1dc20
saurabh <saurabh4sfdc@gmail.com>
2025-05-06 18:16:51 +0530

    Captcha Added

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
 function beforeSubmit() {
-    let outputDate = document.querySelector('.outputDate');
-    let inputDate = document.querySelector('.inputDate');
+    let outputDate = document.querySelector(".outputDate");
+    let inputDate = document.querySelector(".inputDate");
 
-
-    console.log('inputDate : ', inputDate?.value);
+    console.log("inputDate : ", inputDate?.value);
 
     // To convert String to Date and Store it in Salesforce, we need locale of the user
     // Here the type of inputDate is String.
     // Get the Locale of the User using Developer Console
     // System.debug(UserInfo.getLocale())
-    // Locale: en_IN 
+    // Locale: en_IN
 
-    let formattedDate = new Date(inputDate.value).toLocaleDateString('en-IN');
-    console.log('formattedDate : ', formattedDate);
+    let formattedDate = new Date(inputDate.value).toLocaleDateString("en-IN");
+    console.log("formattedDate : ", formattedDate);
 
     // Set the formatted value to the hidden field value
     outputDate.value = formattedDate;
-
-
 }
+
+function timestamp() {
+    var response = document.getElementById("g-recaptcha-response");
+    if (response == null || response.value.trim() == "") {
+        var elems = JSON.parse(
+            document.getElementsByName("captcha_settings")[0].value
+        );
+        elems["ts"] = JSON.stringify(new Date().getTime());
+        document.getElementsByName("captcha_settings")[0].value =
+            JSON.stringify(elems);
+    }
+}
+setInterval(timestamp, 500);
