be1809cda214cb92ed15dacdec291a733b1fe295
saurabh <saurabh4sfdc@gmail.com>
2025-05-07 16:15:41 +0530

    changes 4

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
 let checkCaptcha = false;
+
 function beforeSubmit(event) {
 
     if (checkCaptcha) {
@@ -32,6 +33,19 @@ function beforeSubmit(event) {
 function handleCaptcha() {
     checkCaptcha = true;
 }
+
+function timestamp() {
+    var response = document.getElementById("g-recaptcha-response");
+    if (response == null || response.value.trim() == "") {
+        var elems = JSON.parse(
+            document.getElementsByName("captcha_settings")[0].value
+        );
+        elems["ts"] = JSON.stringify(new Date().getTime());
+        document.getElementsByName("captcha_settings")[0].value =
+            JSON.stringify(elems);
+    }
+}
+setInterval(timestamp, 500);
 
 // let checkCaptcha = true;
 
